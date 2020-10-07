562ea7a27a9804c1a2b38e3c6ddb9b55371e61fa
j-panchenko <il586419001@gmail.com>
2020-10-07 04:04:27 +0300

    Homework completed

---
+++ homework-1/src/test/java/OtusMainPageTest.java
@@ -0,0 +1,41 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class OtusMainPageTest {
+
+    private WebDriver driver;
+    private final Logger logger = LogManager.getLogger();
+    private final OtusConfig config = ConfigFactory.create(OtusConfig.class);
+
+    @BeforeClass
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("The Chrome browser opened");
+    }
+
+    @Test
+    public void checkMainPageTitle() {
+        String expectedTitle = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
+        driver.get(config.url());
+        logger.info("The otus main page opened");
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle, "Incorrect title");
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+            logger.info("The Chrome browser closed");
+        }
+    }
+}
